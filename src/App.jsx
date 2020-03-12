03999d515013c7ed925331c7e6cd49118e176774
Dylan Manley <dylan.manley@gmail.com>
2020-03-12 14:15:29 -0700

    add routing to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Cart from './components/cart'
+import Art from './components/ArtPage';
+import LandingPage from './components/LandingPage';
+import Home from './components/HomePage';
+import Error404 from './components/Error404';
 
 class App extends Component {
   render() {
     return (
+      <>
+      <div>
+      <Switch>
+        <Route exact path='/' component={LandingPage} />
+        <Route path='/home' component={Home} />
+        <Route path='/art' component={Art} />
+        <Route component={Error404} />
+      </Switch>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+          <Cart />
+          </div>
         </header>
-        <div>
-        <Cart />
-        </div>
       </div>
+      </div>
+      </>
     );
   }
 }
