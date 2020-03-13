9d0d88cc3a5161ab408f845cab4498ae36697cbe
Dylan Manley <dylan.manley@gmail.com>
2020-03-12 17:09:05 -0700

    add styling to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
 import { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import Cart from './components/cart'
-import Art from './components/ArtPage';
 import LandingPage from './components/LandingPage';
 import Home from './components/HomePage';
 import Error404 from './components/Error404';
 import Example from './components/Example';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <>
-      <div>
-      <Switch>
-        <Route exact path='/' component={LandingPage} />
-        <Route path='/home' component={Home} />
-        <Route path='/art' component={Art} />
-        <Route path='/example' component={Example} />
-        <Route component={Error404} />
-      </Switch>
-      </div>
+        <div>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route path='/home' component={Home} />
+            <Route path='/example' component={Example} />
+            <Route component={Error404} />
+          </Switch>
+        </div>
       </>
     );
-  }
-}
+
+
 
 export default App;
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import start from './../assets/images/start-image.png'
+import fBnW from './../assets/images/FbNw-trans.png'
+import './../styles/landingPage.css'
 
 function LandingPage(){
   return (
   <div id='landingPage'>
-    <div>
+    <div className='landingImgs'>
+      <img className='fTrans' src={fBnW}/>
       <a href='#/home'><img className='startImgBtn' src={start}/></a>
     </div>
   </div>
